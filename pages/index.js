5b7507608552914058ffb2793852e60f04a6bfc4
ricardopasini <ricardo.pasini@gmail.com>
2025-11-28 13:23:07 +0000

    Changed challenge message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home () {
-    return <h1>Livrai-nos dos narcisistas, AMÉM!</h1>
+    return <div>
+                <h1>Bom dia meu bem!<br></br>
+                Que você consiga focar em você e não pensar em mais ninguém!<br></br>
+                Enche a Francesca de beijos, dê um por mim também!<br></br>
+                E livrai-nos dos narcisistas, AMÉM!</h1>
+            </div>
 }
 
 export default Home;
